6113702a5800e81d337ad2c6e6b7034389df6cac
Wojciech <swirjestem@hotmail.com>
2026-09-16 07:46:11 +0200

    feat(platform): expose platform backend contracts

---
+++ src/swirui/platforms/__init__.py
@@ -0,0 +1,5 @@
+"""Operating-system backend contracts."""
+
+from .base import DisplayInfo, NullPlatformBackend, PlatformBackend
+
+__all__ = ["DisplayInfo", "NullPlatformBackend", "PlatformBackend"]
