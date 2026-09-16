0f50f83de5d06d7b3a47da7b6e79cfb364955cb2
Wojciech <swirjestem@hotmail.com>
2026-09-16 10:55:16 +0200

    feat(app): select best available renderer automatically

--- src/swirui/app.py
+++ src/swirui/app.py
@@ -15,7 +15,7 @@ from .platforms import (
     PlatformEventKind,
     create_platform_backend,
 )
-from .rendering import FrameScheduler, NullRenderer, Renderer
+from .rendering import FrameScheduler, Renderer, create_renderer
 from .window import Window
 
 
@@ -35,7 +35,7 @@ class App(EventEmitter):
         self.config = config or AppConfig()
         self.logger = configure_logging(debug=self.config.debug)
         self.platform_backend = platform_backend or create_platform_backend()
-        self.renderer = renderer or NullRenderer()
+        self.renderer = renderer or create_renderer()
         self.windows: list[Window] = []
         self.running = False
         self.exit_code = 0
