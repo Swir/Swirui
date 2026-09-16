177ac0b063256a1560b478f580ab704dc6228b5c
Wojciech <swirjestem@hotmail.com>
2026-09-16 14:56:30 +0200

    feat(images): export image resources

--- src/swirui/rendering/__init__.py
+++ src/swirui/rendering/__init__.py
@@ -3,6 +3,7 @@
 from .base import NullRenderer, Renderer
 from .factory import create_renderer
 from .geometry import Color, CornerRadius, Point, Rect, Size
+from .resources import ImageResource
 from .scene import Scene, SceneNode, SceneNodeKind
 from .scheduler import FrameScheduler, FrameStats
 from .surface import RenderSurface
@@ -15,6 +16,7 @@ __all__ = [
     "CornerRadius",
     "FrameScheduler",
     "FrameStats",
+    "ImageResource",
     "NullRenderer",
     "Point",
     "Rect",
