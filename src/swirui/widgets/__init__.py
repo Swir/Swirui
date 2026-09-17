37e1e8d1964766cf593efe177aa96bc691f3e091
Wojciech <swirjestem@hotmail.com>
2026-09-17 21:02:24 +0200

    Export Badge Chip and Tooltip widgets

--- src/swirui/widgets/__init__.py
+++ src/swirui/widgets/__init__.py
@@ -6,12 +6,16 @@ from .input import Input, PasswordInput, TextArea
 from .progress import ProgressBar, ProgressRing
 from .runtime import SceneRenderable, WidgetRuntime, compile_component_scene, mount
 from .slider import RangeSlider, Slider
+from .status import Badge, Chip
 from .text import Label, Text
 from .toggle import Checkbox, RadioButton, Switch
+from .tooltip import Tooltip
 
 __all__ = [
+    "Badge",
     "Button",
     "Checkbox",
+    "Chip",
     "IconButton",
     "Input",
     "Label",
@@ -25,6 +29,7 @@ __all__ = [
     "Switch",
     "Text",
     "TextArea",
+    "Tooltip",
     "Widget",
     "WidgetRuntime",
     "compile_component_scene",
