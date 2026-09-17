88a33393c4e24801428919ad165d879f531e69ad
Wojciech <swirjestem@hotmail.com>
2026-09-17 05:57:58 +0200

    feat(rendering): export linear gradient primitives

--- src/swirui/rendering/__init__.py
+++ src/swirui/rendering/__init__.py
@@ -3,6 +3,7 @@
 from .base import NullRenderer, Renderer
 from .factory import create_renderer
 from .geometry import Color, CornerRadius, Path2D, Point, Rect, Size
+from .gradients import GradientStop, LinearGradient
 from .scene import Scene, SceneNode, SceneNodeKind
 from .scheduler import FrameScheduler, FrameStats
 from .surface import RenderSurface
@@ -15,6 +16,8 @@ __all__ = [
     "CornerRadius",
     "FrameScheduler",
     "FrameStats",
+    "GradientStop",
+    "LinearGradient",
     "NullRenderer",
     "Path2D",
     "Point",
