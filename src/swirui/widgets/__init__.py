deda71cc17a153b52f1e06bcbe20b9d2243ed6ac
Wojciech <swirjestem@hotmail.com>
2026-09-17 20:04:23 +0200

    Export retained toggle widgets

--- src/swirui/widgets/__init__.py
+++ src/swirui/widgets/__init__.py
@@ -5,14 +5,18 @@ from .button import Button, IconButton
 from .input import Input, PasswordInput, TextArea
 from .runtime import SceneRenderable, WidgetRuntime, compile_component_scene, mount
 from .text import Label, Text
+from .toggle import Checkbox, RadioButton, Switch
 
 __all__ = [
     "Button",
+    "Checkbox",
     "IconButton",
     "Input",
     "Label",
     "PasswordInput",
+    "RadioButton",
     "SceneRenderable",
+    "Switch",
     "Text",
     "TextArea",
     "Widget",
