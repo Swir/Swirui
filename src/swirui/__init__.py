55b3cac24f56c2b111d72fae4dceb0a72b75aac2
Wojciech <swirjestem@hotmail.com>
2026-09-18 15:25:13 +0200

    Export retained visual transitions

--- src/swirui/__init__.py
+++ src/swirui/__init__.py
@@ -55,6 +55,7 @@ from .interaction_animation import (
 )
 from .page_transition import PageTransition, PageTransitionDirection
 from .shared_element_transition import SharedElementTransition
+from .visual_transition import FadeTransition, SlideTransition
 from .widgets import (
     Accordion,
     AdaptiveNavigation,
@@ -158,6 +159,7 @@ __all__ = [
     "DockPanel",
     "DynamicTypography",
     "Expander",
+    "FadeTransition",
     "Flow",
     "FlowDirection",
     "Frame",
@@ -203,6 +205,7 @@ __all__ = [
     "Row",
     "ScrollView",
     "SharedElementTransition",
+    "SlideTransition",
     "Slider",
     "SplitView",
     "SpringAnimation",
